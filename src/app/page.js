c292f8c9a9d6f0b43581d65d7c70f061c2e8bc89
khkim <khkim@gaia3d.com>
2023-10-11 15:40:59 +0900

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,105 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import {ENTER_KEY} from '@/type/constant';
+import {useMemo, useState} from 'react';
+import {STATUS} from '@/type/status';
+import {v4} from 'uuid';
+import styles from '@/style/page.module.css';
+
+const Home = () => {
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([]);
+  const [status, setStatus] = useState(STATUS.ALL);
+  const onKeyDownAddItem = (event) => {
+    if (event.key !== ENTER_KEY) return;
+
+    setItems([...items, {
+      id: v4(),
+      label: newItem,
+      status: STATUS.ACTIVE
+    }]);
+    setNewItem('');
+  };
+  const filteredItems = useMemo(() => {
+    if (status === STATUS.ALL) return items;
+
+    return items.filter(item => item.status === status);
+  }, [items, status]);
+  const setItemsStatus = () => {
+    const toCompleteAll = !completeAllFlag;
+    setItems(items => items.map(item => ({
+      ...item,
+      status: toCompleteAll ? STATUS.COMPLETE : STATUS.ACTIVE
+    })));
+  };
+  const completeAllFlag = useMemo(() => {
+    return items.every(item => item.status === STATUS.COMPLETE);
+  }, [items]);
+  const onChangeItemStatus = (id, checked) => {
+    setItems(items => items.map(item => ({
+      ...item,
+      status: item.id === id
+        ? checked ? STATUS.COMPLETE : STATUS.ACTIVE
+        : item.status
+    })));
+  };
+  const clearCompletedItems = () => {
+    setItems(items => items.filter(item => item.status !== STATUS.COMPLETE));
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <main>
+      <h1>todos</h1>
+      <div>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          {
+            items.length
+              ? <input type="checkbox"
+                  checked={completeAllFlag}
+                  onChange={() => setItemsStatus()}/>
+              : null
+          }
+          <input
+            value={newItem}
+            onKeyDown={e => onKeyDownAddItem(e)}
+            onChange={e => setNewItem(e.target.value)}/>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div>
+          <ul>
+            {
+              filteredItems.map(item => {
+                return <li key={item.id}>
+                  <input type="checkbox"
+                         checked={item.status === STATUS.COMPLETE}
+                         onChange={(e) => onChangeItemStatus(item.id, e.target.checked)}/>
+                  <span className={item.status === STATUS.COMPLETE ? styles.completedItem : ''}>{item.label}</span>
+                </li>
+              })
+            }
+          </ul>
+        </div>
+        <div>
+          <span>
+            {items.filter(item => item.status === STATUS.ACTIVE).length} item left
+          </span>
+          {
+            Object.values(STATUS).map(value => {
+              return <button key={value}
+                className={status === value ? styles.selectedButton : ''}
+                onClick={() => setStatus(value)}>{value}</button>
+            })
+          }
+          {
+            items.filter(item => item.status === STATUS.COMPLETE).length
+            ? <button onClick={() => clearCompletedItems()}>Clear completed</button>
+            : ''
+          }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   )
-}
+};
+
+export default Home;
---
+++ src/type/status.js
@@ -0,0 +1,5 @@
+export const STATUS = {
+  ALL: 'All',
+  ACTIVE: 'Active',
+  COMPLETE: 'Complete'
+};
